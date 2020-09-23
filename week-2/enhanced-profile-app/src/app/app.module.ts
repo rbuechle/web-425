437bde484afb5bdcb2266f73859090474e750b90
Becca <rbuechle@my365.bellevue.edu>
2020-09-23 03:57:03 -0600

    week2

---
+++ week-2/enhanced-profile-app/src/app/app.module.ts
@@ -0,0 +1,30 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author:  Peter Itskovich
+; Date: September 15 2020
+; Modified By: Becca Buechle
+; Description: component
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
