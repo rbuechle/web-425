3515c234c6d347c7a9107333a7cc536b0303c003
Becca <rbuechle@my365.bellevue.edu>
2020-09-30 17:52:24 -0600

    week 3

---
+++ week-3/enhanced-profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,49 @@
+/*
+============================================
+; Title: my-details.component.ts
+; Author:  Peter Itskovich
+; Date: September 15 2020
+; Modified By: Becca Buechle
+; Description: component
+;===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+export default class Person {
+  fullName: string;
+  favoriteFood: string;
+  favoriteColor: string;
+  keywords = [
+    "#TypeScript", "#2020", "#CodingWithAngular", "#ngOmaha"
+  ];
+
+  constructor(fullName: string, favoriteFood: string, favoriteColor: string) {
+    this.fullName = fullName;
+    this.favoriteFood = favoriteFood;
+    this.favoriteColor = favoriteColor;
+  }
+
+  toString() {
+    console.log(`\n  Full Name:  ${this.fullName}\n  Favorite Food:  ${this.favoriteFood}\n  Favorite Color:  ${this.favoriteColor}`);
+  }
+}
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  myProfile: Person;
+
+  constructor() {
+    this.myProfile = new Person("Becca Buechle", "Steak", "Blue");
+    this.myProfile.toString();
+   }
+
+  ngOnInit(): void {
+  }
+
+}
