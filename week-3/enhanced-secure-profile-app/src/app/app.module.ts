4233c61f8bba4eb4b07372192ec8c90e6e28fbc6
Becca <rbuechle@my365.bellevue.edu>
2020-09-30 18:44:16 -0600

    week 3

---
+++ week-3/enhanced-secure-profile-app/src/app/app.module.ts
@@ -0,0 +1,37 @@
+/*
+============================================
+; Title: Assignment 1.5
+; Author:  Peter Itskovich
+; Date: September 15 2020
+; Modified By: Becca Buechle
+; Description: component
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MyImageComponent } from './my-image/my-image.component';
+import { MyDetailsComponent } from './my-details/my-details.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { HomeComponent } from './home/home.component';
+import { AppRoutes } from './app.routing';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyImageComponent,
+    MyDetailsComponent,
+    SignInComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-3/enhanced-secure-profile-app/src/app/home/home.component.ts
@@ -0,0 +1,30 @@
+ /*
+=======================================
+/ Title: Assignment 3.3
+/ Author: Professor Itskovish
+/ Date: 9/29/20
+/ Modified by: Becca Buechle
+/ Description: Assignment 3.3
+=======================================
+*/
+
+
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+
+export class HomeComponent implements OnInit {
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
