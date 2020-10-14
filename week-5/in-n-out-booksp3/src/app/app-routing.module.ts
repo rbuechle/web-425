f48bec6361c0c0ebe08f65a1b6f192641957d37f
Becca <rbuechle@my365.bellevue.edu>
2020-10-13 20:45:44 -0600

    week 5

---
+++ week-5/in-n-out-booksp3/src/app/app-routing.module.ts
@@ -0,0 +1,42 @@
+  /*
+============================================
+; Title: Navigation and Layout
+; Author:  Peter Itskovich
+; Date: October 12, 2020
+; Modified By: Becca Buechle
+; Description: Assignment 5.2 – Navigation and Layout
+;===========================================
+*/
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { BookListComponent } from './book-list/book-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: BookListComponent
+  },
+  {
+    path: 'book-list',
+    component: BookListComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ week-5/in-n-out-booksp3/src/app/book-list/book-list.component.ts
@@ -0,0 +1,38 @@
+  /*
+============================================
+; Title: Data Tables
+; Author:  Peter Itskovich
+; Date: October 12, 2020
+; Modified By: Becca Buechle
+; Description: Assignment 5.3 – Data Tables
+;===========================================
+*/
+
+import { Component, OnInit } from '@angular/core';
+import { BooksService } from '../books.service';
+import { IBook } from '../book.interface';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+
+  books: Observable<IBook[]>;
+  header: Array<string> = ['isbn', 'title', 'numOfPages', 'authors']
+  book: IBook;
+
+  constructor(private booksService: BooksService) {
+    this.books = this.booksService.getBooks();
+  }
+
+  ngOnInit(): void {
+  }
+
+  showBookDetails(isbn: string) {
+    this.book = this.booksService.getBook(isbn);
+    console.log(this.book);
+  }
+}
